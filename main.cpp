3c6833faab5280b0feae405fe06f93748e191f69
vctrubio <vctrubio@gmail.com>
2023-01-25 23:35:30 +0100

    pseudo code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include "Headers.hpp"
 
 
+/*
+ * socket() - returns a socket descriptor that represents the endpoint
+ * bind() - after socket descriptor, bind unique name to the socket. Servers must bind a name to be accesible from the network
+ * listen() - API indicates a willingness to accept connection requests
+ * accept() - connect() to establish a connection, accept() to accept connection. 
+ * send and receive functions - send() recv() read() write() ect.
+ * close() - release any system resources acquired by the socket
+ */
 
 int main()
 {
