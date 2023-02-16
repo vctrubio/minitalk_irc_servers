73a5c31fb1790af70b96cc6a9cb401ca332ddb8b
vctrubio <vctrubio@gmail.com>
2023-02-16 11:34:06 +0100

    lets do something right

---
+++ v5victhedick/Client.cpp
@@ -0,0 +1,10 @@
+#include "Client.hpp"
+
+Client::Client()
+{
+	_name = "Unamed";
+	_refresh = false;
+}
+
+Client::~Client()
+{}
---
+++ v5victhedick/Client.hpp
@@ -0,0 +1,30 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include "Headers.hpp"
+
+class Channel;
+
+class Client
+{
+	string	_name;
+	string	_mssg;
+	bool	_refresh;
+
+    list<Channel *>              _channels; //dont need it.
+
+public:
+	Client();
+	Client(string name): _name(name), _refresh(false){};
+	~Client();
+	
+	string	rtnName() {return _name;};
+	
+	void	putMssg(string str) {_mssg = str; _refresh = true;};
+	string	rtnMssg() {_refresh = false; return _mssg;};
+	void	refresh(){_refresh = false;};
+	bool	status() {return _refresh;};
+	list<Channel *>	 channels(){return _channels;};
+};
+
+#endif
